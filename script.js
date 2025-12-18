2543fdae25c035a400e0b9d551554cf0f27044d4
Cece8888 <nicolosb67@gmail.com>
2025-12-18 21:48:37 +0100

    small chenges

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ input = document.getElementById("code");
 
 button = document.getElementById("confirm");
 
+error = document.getElementById("error")
+
 loc = "wheel.html";
 
 codes = ["FB34TR", "2334CC", "M324B0", "88T12B", "M32453", "343G8B", "LL1234", "CAA890","YGD839", "789M23", "21SN23"]
@@ -12,8 +14,9 @@ button.addEventListener("click", () => {
 
     if(codes.includes(input.value)){
         window.location.assign(`${loc}?code=${encodeURIComponent(input.value)}`);
+        error.style.display = "none";
     } else {
-        // errore
+        error.style.display = "block";
     }
 
 });
