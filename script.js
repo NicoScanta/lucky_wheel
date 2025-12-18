4e7b3b908d8a54ba7482f32906a119193084186c
Cece8888 <nicolosb67@gmail.com>
2025-12-18 21:12:23 +0100

    added fake codes

--- script.js
+++ script.js
@@ -5,11 +5,13 @@ button = document.getElementById("confirm");
 
 loc = "wheel.html";
 
+codes = ["FB34TR", "2334CC", "M324B0", "88T12B", "M32453", "343G8B", "LL1234", "CAA890","YGD839", "789M23", "21SN23"]
+
 button.addEventListener("click", () => {
     console.log(input.value);
 
-    if(input.value === "123456"){
-        window.location.assign(loc);
+    if(codes.includes(input.value)){
+        window.location.assign(`${loc}?code=${encodeURIComponent(input.value)}`);
     } else {
         // errore
     }
--- wheel.js
+++ wheel.js
@@ -1,11 +1,37 @@
 const wheel = document.getElementById("wheel");
 const spinBtn = document.getElementById("spinBtn");
 
+const params = new URLSearchParams(window.location.search);
+const code = params.get("code");
+
 let deg = 0;
 
 spinBtn.addEventListener("click", () => {
-    // genera un angolo casuale tra 500 e 2000 gradi
-    let spin = Math.floor(Math.random() * 1500) + 500;
+
+    
+    // let spin = Math.floor(Math.random() * 1500) + 500;
+    switch(code){
+        case "FB34TR": spin = 1457; break; // fabio
+        case "2334CC": spin = 1300; break; // claudia
+        case "M324B0": spin = 2000; break; // michele
+        case "88T12B": spin = 900; break; // tania
+        case "M32453": spin = 700; break; // matilde
+        case "343G8B": spin = 799; break; // giorgia
+        case "LL1234": spin = 1345; break; // gabriele
+        case "CAA890": spin = 1445; break; // adriana
+        case "YGD839": spin = 850; break; // daria
+        case "789M23": spin = 680; break;  // marco
+        case "21SN23": spin = 600; break; // mi
+        default: spin = 2000; break;
+    }
+
+    // spin = 500;
     deg += spin;
+    console.log("spin " + spin + " deg " + deg)
     wheel.style.transform = `rotate(${deg}deg)`;
+
+    spinBtn.disabled = true;
+    spinBtn.style.display = "none";
+    
+
 });
