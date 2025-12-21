ed96ecb9b0bf4bcf078d12e41136420b5d72768e
Cece8888 <nicolosb67@gmail.com>
2025-12-21 18:11:46 +0100

    chage game mechanics

--- script.js
+++ script.js
@@ -7,13 +7,16 @@ error = document.getElementById("error")
 
 loc = "wheel.html";
 
-codes = ["FB34TR", "2334CC", "M324B0", "88T12B", "M32453", "343G8B", "LL1234", "CAA890","YGD839", "789M23", "21SN23"]
+codes = ["fabio", "claudia", "adriana", "gabriele", "daria", "michele", "nicolo", "tania","matilde", "giorgia", "marco"]
 
 button.addEventListener("click", () => {
-    console.log(input.value);
 
-    if(codes.includes(input.value)){
-        window.location.assign(`${loc}?code=${encodeURIComponent(input.value)}`);
+    const value = input.value
+    .toLowerCase()        
+    .replace(/\s+/g, ""); // rimuove TUTTI gli spazi
+
+    if(codes.includes(value)){
+        window.location.assign(`${loc}?code=${encodeURIComponent(value)}`);
         error.style.display = "none";
     } else {
         error.style.display = "block";
--- wheel.js
+++ wheel.js
@@ -8,38 +8,95 @@ const win = document.getElementById("win");
 const lose = document.getElementById("lose");
 
 let deg = 0;
+let spin = 0;
+let bool_lose = true;
+let numbers_spin = 0
+let no_show_button = false;
 
 
-TEXT = ["Bancarotta! non è cambiato nulla alla fine!", 
-        " ", 
-        "Hai già troppi orologi ", 
-        " ",
-        " ",
-        "Non sarà una macchina da cucire industriale ma nulla di niente",
-        " ",
-        "HAI VINTO! 2000 EURO SI VA IN VACANZA!",
-        " ",
-        " ",
-        " "]
+TEXT = ["Riprova!", 
+        "Hai perso niente regali per quest'anno!", 
+        "Aspetta! non ha ancora finito di girare", 
+        "Hai vinto il regalo con il simbolo: 🎉", // marco
+        "Hai vinto il regalo con il simbolo: 💌", // lele
+        "Hai vinto il regalo con il simbolo: 🕊️", // matilde
+        "Hai vinto il regalo con il simbolo: 💘", // claudia
+        "Hai vinto il regalo con il simbolo: 🥂", // giorgia
+        "Hai vinto il regalo con il simbolo: 🔔", // adriana
+        "Hai vinto il regalo con il simbolo: 🎁", // daria
+        "Hai vinto il regalo con il simbolo: 💕"] // nicolo
 
+
+function lose_(){
+    spin = 1457;
+}
+
+
+function lele(){
+    wheel.src = "img/lele_wheel.png";
+    win.innerText = TEXT[4];
+    bool_lose = false;
+    spin = 13100;
+}
+
+function bankrupt(){
+    // finisce in bancarotta! 
+    
+    switch(numbers_spin){
+        case 0: spin = 1009; lose.innerText = TEXT[0]; numbers_spin++; break; // skip
+        case 1: spin = 1982; lose.innerText = TEXT[0];numbers_spin++;break; // skip
+        case 2: spin = 3779; lose.innerText = TEXT[0];numbers_spin++;break; //skip
+        default: spin = 12149; lose.innerText = TEXT[1];numbers_spin++; spinBtn.style.display = "none"; break; // bankrupt
+    }
+
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function infinite_spin(set) {
+    spin = set;
+    deg = 0;
+    
+    no_show_button = true;
+    lose.innerText = TEXT[2];
+
+    switch(set){
+        case 1210: win.innerText = TEXT[5]; break; // matilde
+        case 475: win.innerText = TEXT[3]; break; // marco
+    }
+
+    for (let i = 0; i < 5; i++) {
+        spin += i;
+        deg += spin;
+
+        console.log("spin " + spin + " deg " + deg);
+        wheel.style.transform = `rotate(${deg}deg)`;
+
+        if(i != 0)
+            await sleep(3999);
+    }
+    bool_lose = false;    
+}
 
 spinBtn.addEventListener("click", () => {
 
-    let spin = 0;
-    
+    lose.style.display="none";
+
     // let spin = Math.floor(Math.random() * 1500) + 500;
     switch(code){ 
-        case "FB34TR": spin = 1457; lose.innerText = TEXT[0]; break; // fabio bancarotta
-        case "2334CC": spin = 1300; lose.innerText = TEXT[1]; break; // claudia
-        case "M324B0": spin = 2000; lose.innerText = TEXT[2]; break; // michele
-        case "88T12B": spin = 900; lose.innerText = TEXT[3]; break; // tania
-        case "M32453": spin = 700; lose.innerText = TEXT[4]; break; // matilde
-        case "343G8B": spin = 799; lose.innerText = TEXT[5]; break; // giorgia
-        case "LL1234": spin = 1345; lose.innerText = TEXT[6]; break; // gabriele
-        case "CAA890": spin = 1445; win.innerText = TEXT[7]; break; // adriana 2000
-        case "YGD839": spin = 850; lose.innerText = TEXT[8]; break; // daria
-        case "789M23": spin = 680; lose.innerText = TEXT[9];break;  // marco
-        case "21SN23": spin = 600; lose.innerText = TEXT[10]; break; // mi
+        case "fabio": bankrupt(); break; // fabio bancarotta
+        case "claudia": spin = 1300; win.innerText = TEXT[6]; bool_lose = false; break; // claudia
+        case "michele": bankrupt(); break; // michele
+        case "tania": bankrupt(); break; // tania
+        case "matilde": infinite_spin(1210); break; // matilde
+        case "giorgia": spin = 910; win.innerText = TEXT[7]; bool_lose = false; break; // giorgia
+        case "gabriele": lele(); break; // gabriele
+        case "adriana": spin = 1445; win.innerText = TEXT[8]; bool_lose = false; break; // adriana 2000
+        case "daria": spin = 874; win.innerText = TEXT[9]; bool_lose = false; break; // daria
+        case "marco": infinite_spin(475); break;  // marco
+        case "nicolo": spin = 1610; win.innerText = TEXT[10]; bool_lose = false; break; // mi
         default: spin = 2000; lose.innerText = "come e' possibile!? non dovresti essere qua"; break;
     }
 
@@ -48,7 +105,7 @@ spinBtn.addEventListener("click", () => {
     console.log("spin " + spin + " deg " + deg);
     wheel.style.transform = `rotate(${deg}deg)`;
 
-    spinBtn.disabled = true;
+    //spinBtn.disabled = true;
     spinBtn.style.display = "none";
     
 
@@ -56,15 +113,20 @@ spinBtn.addEventListener("click", () => {
 
 
 wheel.addEventListener("transitionend", () => {
-
-    if(win.innerText != null){
+    
+    if(bool_lose == false){
+     lose.style.display = "none";
      win.style.display = "block";
      launchConfetti(); 
     } else {
+        if(!no_show_button)
+            spinBtn.style.display = "block";
+
         lose.style.display="block";
     }
+    
 
-}, { once: true });
+});
 
 
 function launchConfetti(count = 120) {
